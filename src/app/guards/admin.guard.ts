f34ce13489a29ffcfb47bfe9bdb5f8f780bf9850
Gerardo <ggutierrez@grainchain.io>
2020-04-21 20:00:26 -0500

    Fixing guard redirection

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 import { AuthService } from '../core/services/auth.service';
 
@@ -10,13 +10,14 @@ import { AuthService } from '../core/services/auth.service';
 })
 export class AdminGuard implements CanActivate {
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree | any> | Promise<boolean | UrlTree> | boolean | UrlTree {
       return this.authService.hasUser().pipe(
-        map(user => user)
+        map(user => user),
+        tap(hasUser => !hasUser && this.router.navigate(['/auth/login']))
       );
     }
 }
