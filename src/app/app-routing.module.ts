add9e5fe49f81aa411f8291b555da9e720c18279
Gerardo <ggutierrez@grainchain.io>
2020-04-21 19:29:58 -0500

    Firebase configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -35,6 +35,10 @@ const routes: Routes = [
         loadChildren: () => import('./order/order.module').then(m => m.OrderModule),
       },
       {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+      },
+      {
         path: 'demo',
         loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule),
         canActivate: [AdminGuard]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  url_api: 'https://platzi-store.herokuapp.com'
+  url_api: 'https://platzi-store.herokuapp.com',
+  firebaseConfig: {
+    apiKey: 'AIzaSyDGpy492AqYdBCNwM8qljaKOS0s1Wwx5uY',
+    authDomain: 'store-5bb8d.firebaseapp.com',
+    databaseURL: 'https://store-5bb8d.firebaseio.com',
+    projectId: 'store-5bb8d',
+    storageBucket: 'store-5bb8d.appspot.com',
+    messagingSenderId: '39409421564',
+    appId: '1:39409421564:web:838093d8cdd5362dc11b79'
+  }
 };
